6856bb05112e47a3789a54b8f33ec88c1fd7f340
Fazal Ur Rehman <fazal.rehman@techsembly.com>
2023-07-16 12:56:38 +0500

    test on live http

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ function renderTodos(todos) {
     li.id = todo.id;
     li.textContent = todo.text;
 
-    // Toggle 'completed' class based on todo.completed property
+
     if (todo.completed) {
       li.classList.add('completed');
     }
@@ -22,11 +22,8 @@ function renderTodos(todos) {
 
     todoList.prepend(li);
   });
+
 }
-todoForm.addEventListener('submit', e => {
-  e.preventDefault();
-  addTodo();
-});
 
 function addTodo() {
   const todoText = todoInput.value.trim();
@@ -61,10 +58,7 @@ function addTodo() {
   } else {
     console.error('Todo text is empty');
   }
-  preventDefault()
 }
-
-
 
 function updateTodoStatus(todoId) {
   fetch(`http://localhost:3000/todos/${todoId}`)
@@ -72,13 +66,13 @@ function updateTodoStatus(todoId) {
     .then(todo => {
       const updatedCompleted = !todo.completed; // Toggle the completed property
       return fetch(`http://localhost:3000/todos/${todoId}`, {
-        method: 'PATCH',
-        headers: {
-          'Content-Type': 'application/json'
-        },
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
         body: JSON.stringify({ completed: updatedCompleted })
       });
-    })
+  })
     .then(response => response.json())
     .then(updatedTodo => {
       const li = document.getElementById(updatedTodo.id);
@@ -88,7 +82,6 @@ function updateTodoStatus(todoId) {
       console.error('Error updating todo:', error);
     });
 }
-
 
 function deleteTodo(todoId) {
   fetch(`http://localhost:3000/todos/${todoId}`, {
@@ -102,8 +95,21 @@ function deleteTodo(todoId) {
       console.error('Error deleting todo:', error);
     });
 }
+function updateTodoStatus(todoId) {
+  const selectedTodo = todos.find(todo => todo.id === todoId);
+  if (selectedTodo) {
+    selectedTodo.completed = !selectedTodo.completed; // Toggle the completed status
+    saveTodos();
+    renderTodos();
+  }
+}
 
-// Update todo status when li element is clicked
+
+todoForm.addEventListener('submit', e => {
+  e.preventDefault();
+  addTodo();
+});
+
 todoList.addEventListener('click', e => {
   if (e.target.tagName === 'LI') {
     const todoId = parseInt(e.target.id);
@@ -114,7 +120,6 @@ todoList.addEventListener('click', e => {
   }
 });
 
-// Fetch todos from the server and render them
 fetch('http://localhost:3000/todos')
   .then(response => response.json())
   .then(todos => {
@@ -123,3 +128,12 @@ fetch('http://localhost:3000/todos')
   .catch(error => {
     console.error('Error retrieving todos:', error);
   });
+
+
+
+
+
+
+
+
+  
